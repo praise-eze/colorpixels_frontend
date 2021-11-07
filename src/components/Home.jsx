261fa00cddee527f260eb6768c96fda0e245f5ae
shawn d'souza <shawnkdsouza@outlook.com>
2021-11-06 20:14:22 -0400

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,41 @@
 import React from 'react'
 import Slider from "./Slider"
+import { Text, Container, Box, Image, VStack, StackDivider, Grid, Heading } from "@chakra-ui/react"
+
+
 
 const Home = () => {
     return (
         <div>
-        <Slider/>
+        <div>
+            <Slider/>
+        </div>
+        <Grid templateColumns="repeat(2, 1fr)" gap={1}>
+            <Box boxSize="sm" className="">
+                <Image src="https://i.ytimg.com/vi/fwb9JysHYSI/maxresdefault.jpg" alt="adin and boat"></Image>
+            </Box>
             <div>
-        <div>
-    <h1>"Stream Thumbnail"</h1>
-    </div>
-    <h2>Start a collection of your favorite highlights</h2>
-    <p>Explore the marketplace to buy or bid for any clip. Each clip is minted as an NFT, which means you can publically own that clip! Each NFT lives on the Ethereum blockchain.</p>
-</div>
+                <Heading fontSize="4xl">Start a collection of your favorite highlights</Heading>
+                <Text fontSize="2xl">Explore the marketplace to buy or bid for any clip. Each clip is minted as an NFT, which means you can publically own that clip! Each NFT lives on the Ethereum blockchain.</Text>
+            </div>
 
-<div>
-        <div>
-    <h1>"Stream Thumbnail"</h1>
-    </div>
-    <h2>Become a creator and sell custom clips</h2>
-    <p>When you sell a clip, it gets shown on your creator profile. This profile lets you create a community around your creator platform.</p>
-</div>
-<div>
-        <div>
-    <h1>"Stream Thumbnail"</h1>
-    </div>
-    <h2>Earn Pixels</h2>
-    <p>Earn Pixels everytime you get upvoted by the community. Pixels give you access to exclusive content!</p>
-</div>
+            <Box boxSize="sm">
+                <Image src="https://i.ytimg.com/vi/EVCB69WzoqU/maxresdefault.jpg" alt="rice and zias"></Image>
+            </Box>
+            <div>
+                <Heading fontSize="4xl">Become a creator and sell custom clips</Heading>
+                <Text fontSize="2xl">When you sell a clip, it gets shown on your creator profile. This profile lets you create a community around your creator platform.</Text>
+            </div>
+
+ 
+        <Box boxSize="sm">
+                <Image src="https://clips-media-assets2.twitch.tv/AT-cm%7C247853416-social-preview.jpg" alt="nadeshot fortnite"></Image>
+            </Box>
+            <div>
+            <Heading fontSize="4xl">Earn Pixels</Heading>
+            <Text fontSize="2xl">Earn Pixels everytime you get upvoted by the community. Pixels give you access to exclusive content!</Text>
+        </div>
+        </Grid>
         </div>
     )
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Container, VStack, Button, Spacer } from "@chakra-ui/react"
+import { Container, VStack, Button, Spacer, Text, Heading  } from "@chakra-ui/react"
 
 
 
 const Slider = () => {
     return (
         <div>
-        <Container maxW="container.md">
-  <h1>Collect Your Favorite Streaming Highlights</h1>
-  <p>Buy, sell, share, and share those unforgettable moments</p>
-  <Button colorScheme="teal" size="md">Explore the Marketplace</Button>
-</Container>
+        <Container maxW="container.xl">
+            <Heading fontSize="6xl">Collect Your Favorite Streaming Highlights</Heading>
+            <Text fontSize="lg">Buy, sell, share, and share those unforgettable moments</Text>
+            <Button colorScheme="teal" size="md">Explore the Marketplace</Button>
+        </Container>
  
 
-</div>
+        </div>
     )
 }
 
